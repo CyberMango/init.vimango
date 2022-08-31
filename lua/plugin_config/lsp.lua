da729ed882d10e89fe2dd3a8bc06038a236f7251
CyberMango <Cyber@Mango.fakeemail.com>
2022-09-01 01:13:46 +0300

    Made c-n use onmicompletion on lsp attached buffers.

--- lua/plugin_config/lsp.lua
+++ lua/plugin_config/lsp.lua
@@ -28,6 +28,7 @@ local on_attach = function(_, bufnr)
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("i", "<c-n>", "<c-x><c-o>", bufopts)
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
